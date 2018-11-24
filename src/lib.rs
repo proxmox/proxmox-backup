07d3fa7289c6f063e4bf26a4b514c0b62044019e
Dietmar Maurer <dietmar@proxmox.com>
2018-11-24 17:52:43 +0100

    add some inline documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 pub mod static_map;
 
+/// API definition helper
+///
+/// This module contains helper classes to define REST APIs. Method
+/// parameters and return types are described using a
+/// [Schema](schema/enum.Schema.html).
+///
+/// The [Router](router/struct.Router.html) is used to define a
+/// hierarchy of API entries, and provides ways to find an API
+/// definition by path.
+
 #[macro_use]
 pub mod api {
 
