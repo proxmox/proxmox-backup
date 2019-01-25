d1a35593cbec87ebc2167ae494d427b1729c7a40
Dietmar Maurer <dietmar@proxmox.com>
2019-01-25 13:10:55 +0100

    ServerAdministration.js: new widget

---
+++ www/ServerAdministration.js
@@ -0,0 +1,68 @@
+/*global Proxmox*/
+Ext.define('PBS.ServerAdministration', {
+    extend: 'Ext.tab.Panel',
+    alias: 'widget.pbsServerAdministration',
+
+    title: gettext('Server Administration'),
+
+    border: false,
+    defaults: { border: false },
+
+    controller: {
+	xclass: 'Ext.app.ViewController',
+
+        init: function(view) {
+	    var upgradeBtn = view.lookupReference('upgradeBtn');
+	    upgradeBtn.setDisabled(!(Proxmox.UserName && Proxmox.UserName === 'root@pam'));
+	}
+    },
+
+    items: [
+//	{
+//	    xtype: 'pbsServerStatus',
+//	    itemId: 'status'
+//	},
+	{
+	    xtype: 'proxmoxNodeServiceView',
+            title: gettext('Services'),
+	    itemId: 'services',
+	    startOnlyServices: {
+		syslog: true,
+		pmgproxy: true,
+		pmgdaemon: true
+	    },
+	    nodename: Proxmox.NodeName
+	},
+	{
+	    xtype: 'proxmoxNodeAPT',
+            title: gettext('Updates'),
+	    upgradeBtn: {
+		xtype: 'button',
+		reference: 'upgradeBtn',
+		disabled: true,
+		text: gettext('Upgrade'),
+		handler: function() {
+		    Proxmox.Utils.openXtermJsViewer('upgrade', 0, Proxmox.NodeName);
+		}
+	    },
+	    itemId: 'updates',
+	    nodename: Proxmox.NodeName
+	},
+	{
+	    xtype: 'proxmoxLogView',
+	    itemId: 'logs',
+	    title: gettext('Syslog'),
+	    url: "/api2/extjs/nodes/" + Proxmox.NodeName + "/syslog",
+	    log_select_timespan: 1
+	},
+	{
+	    xtype: 'proxmoxNodeTasks',
+	    itemId: 'tasks',
+	    title: gettext('Tasks'),
+	    height: 'auto',
+	    nodename: Proxmox.NodeName
+	}
+    ]
+});
+
+
