99be8844c7c4759e6aed56aca503219be7049fef
Dietmar Maurer <dietmar@proxmox.com>
2019-02-16 12:28:50 +0100

    buildcfg.rs: improve doc

--- src/buildcfg.rs
+++ src/buildcfg.rs
@@ -1,5 +1,16 @@
+//! Exports configuration data from the build system
+
+/// The configured configuration directory
 pub const CONFIGDIR: &'static str = env!("PROXMOX_CONFIGDIR");
 
+/// Prepend configuration directory to a file name
+///
+/// This is a simply way to get the full path for configuration files.
+/// #### Example:
+/// ```
+/// # #[macro_use] extern crate proxmox_backup;
+/// let cert_path = configdir!("/proxy.pfx");
+/// ```
 #[macro_export]
 macro_rules! configdir {
     ($subdir:expr) => (concat!(env!("PROXMOX_CONFIGDIR"), $subdir))
