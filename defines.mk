1ce9eda6323fe13926e9907f9f1b7afa94f26a0e
Wolfgang Bumiller <w.bumiller@proxmox.com>
2019-08-21 12:22:35 +0200

    buildsys: defer some variable expansions
    So that local.mak needs to only contain a PREFIX.
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- defines.mk
+++ defines.mk
@@ -2,15 +2,15 @@ PACKAGE := proxmox-backup
 PKGVER := 0.1
 PKGREL := 1
 
-PREFIX := /usr
-BINDIR := $(PREFIX)/bin
-SBINDIR := $(PREFIX)/sbin
-LIBDIR := $(PREFIX)/lib
-LIBEXECDIR := $(LIBDIR)
-DATAROOTDIR := $(PREFIX)/share
-MAN1DIR := $(PREFIX)/share/man/man1
-JSDIR := $(DATAROOTDIR)/javascript/proxmox-backup
-SYSCONFDIR := /etc
+PREFIX = /usr
+BINDIR = $(PREFIX)/bin
+SBINDIR = $(PREFIX)/sbin
+LIBDIR = $(PREFIX)/lib
+LIBEXECDIR = $(LIBDIR)
+DATAROOTDIR = $(PREFIX)/share
+MAN1DIR = $(PREFIX)/share/man/man1
+JSDIR = $(DATAROOTDIR)/javascript/proxmox-backup
+SYSCONFDIR = /etc
 
 # For local overrides
 -include local.mak
