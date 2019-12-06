d75fbb0a21b80b9e3dd86d0199ef8db477a1138a
Dietmar Maurer <dietmar@proxmox.com>
2019-12-06 09:53:14 +0100

    src/bin/dump-catalog-shell-cli.rs: generate catalog shell docs

---
+++ src/bin/dump-catalog-shell-cli.rs
@@ -0,0 +1,20 @@
+use failure::*;
+
+use proxmox::api::format::*;
+use proxmox::api::cli::*;
+
+use proxmox_backup::backup::catalog_shell_cli;
+
+fn main() -> Result<(), Error> {
+
+    match catalog_shell_cli() {
+        CommandLineInterface::Nested(map) => {
+            let usage = generate_nested_usage("", &map, DocumentationFormat::ReST);
+            println!("{}", usage);
+
+        }
+        _ => unreachable!(),
+    }
+
+    Ok(())
+}
