a0153b02c99eec52a9cf31ff9ce694de82552796
Dominik Csapak <d.csapak@proxmox.com>
2020-05-29 16:22:14 +0200

    ui: use Proxmox.Utils.setAuthData
    this uses different parameters which we want to be the same for
    all products (e.g. secure cookie)
    
    leave the PBS.Utils.updateLoginData for the case that we want to do
    something more here (as in pve for example)
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/Utils.js
+++ www/Utils.js
@@ -7,12 +7,8 @@ Ext.define('PBS.Utils', {
     singleton: true,
 
     updateLoginData: function(data) {
-	Proxmox.CSRFPreventionToken = data.CSRFPreventionToken;
-	Proxmox.UserName = data.username;
-	//console.log(data.ticket);
-	// fixme: use secure flag once we have TLS
-	//Ext.util.Cookies.set('PBSAuthCookie', data.ticket, null, '/', null, true );
-	Ext.util.Cookies.set('PBSAuthCookie', data.ticket, null, '/', null, false);
+
+	Proxmox.Utils.setAuthData(data);
     },
 
     dataStorePrefix: 'DataStore-',
