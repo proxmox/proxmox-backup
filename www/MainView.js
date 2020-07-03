36cb4b30efd62b53301fbcf8e806700f9207b291
Dominik Csapak <d.csapak@proxmox.com>
2020-07-03 12:48:49 +0200

    add beta text with link to bugtracker
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/MainView.js
+++ www/MainView.js
@@ -200,6 +200,11 @@ Ext.define('PBS.MainView', {
 		    xtype: 'versioninfo'
 		},
 		{
+		    padding: 5,
+		    html: '<a href="https://bugzilla.proxmox.com" target="_blank">BETA</a>',
+		    baseCls: 'x-plain',
+		},
+		{
 		    flex: 1
 		},
 		{
