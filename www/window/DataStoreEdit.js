67042466e84bfea1f82c84dc4580ac1401c4b9e2
Thomas Lamprecht <t.lamprecht@proxmox.com>
2020-07-14 10:56:19 +0200

    ui: datastore edit: avoid an extra indentation level
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/window/DataStoreEdit.js
+++ www/window/DataStoreEdit.js
@@ -21,134 +21,128 @@ Ext.define('PBS.DataStoreEdit', {
 	return {};
     },
 
-    items: [
-	{
-	    xtype: 'tabpanel',
-	    bodyPadding: 10,
-	    items: [
-		{
-		    title: gettext('General'),
-		    xtype: 'inputpanel',
-		    column1: [
-			{
-			    xtype: 'pmxDisplayEditField',
-			    cbind: {
-				editable: '{isCreate}',
-			    },
-			    name: 'name',
-			    allowBlank: false,
-			    fieldLabel: gettext('Name'),
+    items: {
+	xtype: 'tabpanel',
+	bodyPadding: 10,
+	items: [
+	    {
+		title: gettext('General'),
+		xtype: 'inputpanel',
+		column1: [
+		    {
+			xtype: 'pmxDisplayEditField',
+			cbind: {
+			    editable: '{isCreate}',
 			},
-			{
-			    xtype: 'pmxDisplayEditField',
-			    cbind: {
-				editable: '{isCreate}',
-			    },
-			    name: 'path',
-			    allowBlank: false,
-			    fieldLabel: gettext('Backing Path'),
-			    emptyText: gettext('An absolute path'),
+			name: 'name',
+			allowBlank: false,
+			fieldLabel: gettext('Name'),
+		    },
+		    {
+			xtype: 'pmxDisplayEditField',
+			cbind: {
+			    editable: '{isCreate}',
 			},
-		    ],
-
-		    column2: [
-			{
-			    xtype: 'proxmoxtextfield',
-			    name: 'gc-schedule',
-			    fieldLabel: gettext("GC Schedule"),
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
+			name: 'path',
+			allowBlank: false,
+			fieldLabel: gettext('Backing Path'),
+			emptyText: gettext('An absolute path'),
+		    },
+		],
+		column2: [
+		    {
+			xtype: 'proxmoxtextfield',
+			name: 'gc-schedule',
+			fieldLabel: gettext("GC Schedule"),
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-			{
-			    xtype: 'proxmoxtextfield',
-			    name: 'prune-schedule',
-			    fieldLabel: gettext("Prune Schedule"),
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
+		    },
+		    {
+			xtype: 'proxmoxtextfield',
+			name: 'prune-schedule',
+			fieldLabel: gettext("Prune Schedule"),
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-		    ],
-
-		    columnB: [
-			{
-			    xtype: 'textfield',
-			    name: 'comment',
-			    fieldLabel: gettext('Comment'),
+		    },
+		],
+		columnB: [
+		    {
+			xtype: 'textfield',
+			name: 'comment',
+			fieldLabel: gettext('Comment'),
+		    },
+		],
+	    },
+	    {
+		title: gettext('Prune Options'),
+		xtype: 'inputpanel',
+		column1: [
+		    {
+			xtype: 'proxmoxintegerfield',
+			fieldLabel: gettext('Keep Last'),
+			name: 'keep-last',
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-		    ],
-		},
-		{
-		    title: gettext('Prune Options'),
-		    xtype: 'inputpanel',
-		    column1: [
-			{
-			    xtype: 'proxmoxintegerfield',
-			    fieldLabel: gettext('Keep Last'),
-			    name: 'keep-last',
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
-			    minValue: 1,
-			    allowBlank: true,
+			minValue: 1,
+			allowBlank: true,
+		    },
+		    {
+			xtype: 'proxmoxintegerfield',
+			fieldLabel: gettext('Keep Daily'),
+			name: 'keep-daily',
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-			{
-			    xtype: 'proxmoxintegerfield',
-			    fieldLabel: gettext('Keep Daily'),
-			    name: 'keep-daily',
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
-			    minValue: 1,
-			    allowBlank: true,
+			minValue: 1,
+			allowBlank: true,
+		    },
+		    {
+			xtype: 'proxmoxintegerfield',
+			fieldLabel: gettext('Keep Monthly'),
+			name: 'keep-monthly',
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-			{
-			    xtype: 'proxmoxintegerfield',
-			    fieldLabel: gettext('Keep Monthly'),
-			    name: 'keep-monthly',
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
-			    minValue: 1,
-			    allowBlank: true,
+			minValue: 1,
+			allowBlank: true,
+		    },
+		],
+		column2: [
+		    {
+			xtype: 'proxmoxintegerfield',
+			fieldLabel: gettext('Keep Hourly'),
+			name: 'keep-hourly',
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-		    ],
-
-		    column2: [
-			{
-			    xtype: 'proxmoxintegerfield',
-			    fieldLabel: gettext('Keep Hourly'),
-			    name: 'keep-hourly',
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
-			    minValue: 1,
-			    allowBlank: true,
+			minValue: 1,
+			allowBlank: true,
+		    },
+		    {
+			xtype: 'proxmoxintegerfield',
+			fieldLabel: gettext('Keep Weekly'),
+			name: 'keep-weekly',
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-			{
-			    xtype: 'proxmoxintegerfield',
-			    fieldLabel: gettext('Keep Weekly'),
-			    name: 'keep-weekly',
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
-			    minValue: 1,
-			    allowBlank: true,
+			minValue: 1,
+			allowBlank: true,
+		    },
+		    {
+			xtype: 'proxmoxintegerfield',
+			fieldLabel: gettext('Keep Yearly'),
+			name: 'keep-yearly',
+			cbind: {
+			    deleteEmpty: '{!isCreate}',
 			},
-			{
-			    xtype: 'proxmoxintegerfield',
-			    fieldLabel: gettext('Keep Yearly'),
-			    name: 'keep-yearly',
-			    cbind: {
-				deleteEmpty: '{!isCreate}',
-			    },
-			    minValue: 1,
-			    allowBlank: true,
-			},
-		    ],
-
-		}
-	    ]
-	}
-    ],
+			minValue: 1,
+			allowBlank: true,
+		    },
+		],
+	    },
+	],
+    },
 });
