796480a38b145198dc3ca933affe25449fcc2e2a
Thomas Lamprecht <t.lamprecht@proxmox.com>
2020-09-21 18:25:58 +0200

    docs: add version and date to HTML index
    Similar to the PDF output or the Proxmox VE docs.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- docs/conf.py
+++ docs/conf.py
@@ -97,12 +97,10 @@ language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-#
 # today = ''
 #
 # Else, today_fmt is used as the format for a strftime call.
-#
-# today_fmt = '%B %d, %Y'
+today_fmt = '%A, %d %B %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
--- docs/index.rst
+++ docs/index.rst
@@ -2,8 +2,8 @@
 
 Welcome to the Proxmox Backup documentation!
 ============================================
-
-Copyright (C) 2019-2020 Proxmox Server Solutions GmbH
+| Copyright (C) 2019-2020 Proxmox Server Solutions GmbH
+| Version |version| -- |today|
 
 Permission is granted to copy, distribute and/or modify this document under the
 terms of the GNU Free Documentation License, Version 1.3 or any later version
