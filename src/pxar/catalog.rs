6fd129844dae9da6fa0b0729cad1953021ee2ada
Wolfgang Bumiller <w.bumiller@proxmox.com>
2020-09-22 08:59:54 +0200

    remove DummyCatalogWriter
    we're using an `Option` instead now
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- src/pxar/catalog.rs
+++ src/pxar/catalog.rs
@@ -17,17 +17,3 @@ pub trait BackupCatalogWriter {
     fn add_fifo(&mut self, name: &CStr) -> Result<(), Error>;
     fn add_socket(&mut self, name: &CStr) -> Result<(), Error>;
 }
-
-pub struct DummyCatalogWriter();
-
-impl BackupCatalogWriter for DummyCatalogWriter {
-    fn start_directory(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-    fn end_directory(&mut self) -> Result<(), Error> { Ok(()) }
-    fn add_file(&mut self, _name: &CStr, _size: u64, _mtime: u64) -> Result<(), Error> { Ok(()) }
-    fn add_symlink(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-    fn add_hardlink(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-    fn add_block_device(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-    fn add_char_device(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-    fn add_fifo(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-    fn add_socket(&mut self, _name: &CStr) -> Result<(), Error> { Ok(()) }
-}
