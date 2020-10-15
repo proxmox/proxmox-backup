254b1f2213380806118d40f279e8f7ca2ef3f8de
Stefan Reiter <s.reiter@proxmox.com>
2020-10-15 12:49:15 +0200

    rustdoc: add crate level doc
    Contains a link to the 'backup' module's doc, as that explains a lot
    about the inner workings of PBS and probably marks a good entry point
    for new readers.
    
    Signed-off-by: Stefan Reiter <s.reiter@proxmox.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! See the different modules for documentation on their usage.
+//!
+//! The [backup](backup/index.html) module contains some detailed information
+//! on the inner workings of the backup server regarding data storage.
+
 pub mod task;
 
 #[macro_use]
