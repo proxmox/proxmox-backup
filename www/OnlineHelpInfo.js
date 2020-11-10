c8219747f0b4aeaa8b3aba2dfc5b60554a13747c
Aaron Lauterer <a.lauterer@proxmox.com>
2020-11-10 11:45:42 +0100

    ui: add all online help refs found in docs
    recommit the onlinehelp after the scanrefs script has been adapted and
    the docs are up to date
    
    Signed-off-by: Aaron Lauterer <a.lauterer@proxmox.com>

--- www/OnlineHelpInfo.js
+++ www/OnlineHelpInfo.js
@@ -3,6 +3,14 @@ const proxmoxOnlineHelpInfo = {
     "link": "/docs/index.html",
     "title": "Proxmox Backup Server Documentation Index"
   },
+  "id1": {
+    "link": "/docs/calendarevents.html#id1",
+    "title": "Calendar Events"
+  },
+  "id2": {
+    "link": "/docs/backup-client.html#id2",
+    "title": "Encryption"
+  },
   "changing-backup-owner": {
     "link": "/docs/backup-client.html#changing-backup-owner",
     "title": "Changing the Owner of a Backup Group"
@@ -10,6 +18,18 @@ const proxmoxOnlineHelpInfo = {
   "backup-pruning": {
     "link": "/docs/backup-client.html#backup-pruning",
     "title": "Pruning and Removing Backups"
+  },
+  "id3": {
+    "link": "/docs/backup-client.html#id3",
+    "title": "Garbage Collection"
+  },
+  "pxar-format": {
+    "link": "/docs/file-formats.html#pxar-format",
+    "title": "Proxmox File Archive Format (``.pxar``)"
+  },
+  "sysadmin-package-repositories": {
+    "link": "/docs/package-repositories.html#sysadmin-package-repositories",
+    "title": "Debian Package Repositories"
   },
   "get-help": {
     "link": "/docs/introduction.html#get-help",
@@ -19,9 +39,21 @@ const proxmoxOnlineHelpInfo = {
     "link": "/docs/sysadmin.html#chapter-zfs",
     "title": "ZFS on Linux"
   },
+  "local-zfs-special-device": {
+    "link": "/docs/sysadmin.html#local-zfs-special-device",
+    "title": "ZFS Special Device"
+  },
   "maintenance-pruning": {
     "link": "/docs/maintenance.html#maintenance-pruning",
     "title": "Pruning"
+  },
+  "maintenance-gc": {
+    "link": "/docs/maintenance.html#maintenance-gc",
+    "title": "Garbage Collection"
+  },
+  "maintenance-verification": {
+    "link": "/docs/maintenance.html#maintenance-verification",
+    "title": "Verification"
   },
   "maintenance-notification": {
     "link": "/docs/maintenance.html#maintenance-notification",
@@ -35,6 +67,50 @@ const proxmoxOnlineHelpInfo = {
     "link": "/docs/managing-remotes.html#syncjobs",
     "title": "Sync Jobs"
   },
+  "sysadmin-network-configuration": {
+    "link": "/docs/network-management.html#sysadmin-network-configuration",
+    "title": "Network Management"
+  },
+  "pve-integration": {
+    "link": "/docs/pve-integration.html#pve-integration",
+    "title": "`Proxmox VE`_ Integration"
+  },
+  "rst-primer": {
+    "link": "/docs/reStructuredText-primer.html#rst-primer",
+    "title": "reStructuredText Primer"
+  },
+  "rst-inline-markup": {
+    "link": "/docs/reStructuredText-primer.html#rst-inline-markup",
+    "title": "Inline markup"
+  },
+  "rst-literal-blocks": {
+    "link": "/docs/reStructuredText-primer.html#rst-literal-blocks",
+    "title": "Literal blocks"
+  },
+  "rst-doctest-blocks": {
+    "link": "/docs/reStructuredText-primer.html#rst-doctest-blocks",
+    "title": "Doctest blocks"
+  },
+  "rst-tables": {
+    "link": "/docs/reStructuredText-primer.html#rst-tables",
+    "title": "Tables"
+  },
+  "rst-field-lists": {
+    "link": "/docs/reStructuredText-primer.html#rst-field-lists",
+    "title": "Field Lists"
+  },
+  "rst-roles-alt": {
+    "link": "/docs/reStructuredText-primer.html#rst-roles-alt",
+    "title": "Roles"
+  },
+  "rst-directives": {
+    "link": "/docs/reStructuredText-primer.html#rst-directives",
+    "title": "Directives"
+  },
+  "html-meta": {
+    "link": "/docs/reStructuredText-primer.html#html-meta",
+    "title": "HTML Metadata"
+  },
   "storage-disk-management": {
     "link": "/docs/storage.html#storage-disk-management",
     "title": "Disk Management"
@@ -43,9 +119,17 @@ const proxmoxOnlineHelpInfo = {
     "link": "/docs/storage.html#datastore-intro",
     "title": "Datastore"
   },
+  "storage-datastore-create": {
+    "link": "/docs/storage.html#storage-datastore-create",
+    "title": "Creating a Datastore"
+  },
   "user-mgmt": {
     "link": "/docs/user-management.html#user-mgmt",
     "title": "User Management"
+  },
+  "user-tokens": {
+    "link": "/docs/user-management.html#user-tokens",
+    "title": "API Tokens"
   },
   "user-acl": {
     "link": "/docs/user-management.html#user-acl",
