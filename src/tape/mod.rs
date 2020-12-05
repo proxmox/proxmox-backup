c4d2d54a6d8ade40ce71b94c257cbfeb7c300369
Dietmar Maurer <dietmar@proxmox.com>
2020-12-05 12:20:46 +0100

    tape: define useful constants

--- src/tape/mod.rs
+++ src/tape/mod.rs
@@ -5,3 +5,14 @@ pub use tape_write::*;
 
 mod tape_read;
 pub use tape_read::*;
+
+/// Directory path where we stora all status information
+pub const MEDIA_POOL_STATUS_DIR: &str = "/var/lib/proxmox-backup/mediapool";
+
+/// We limit chunk archive size, so that we can faster restore a
+/// specific chunk (The catalog only store file numbers, so we
+/// need to read the whole archive to restore a single chunk)
+pub const MAX_CHUNK_ARCHIVE_SIZE: usize = 4*1024*1024*1024; // 4GB for now
+
+/// To improve performance, we need to avoid tape drive buffer flush.
+pub const COMMIT_BLOCK_SIZE: usize = 128*1024*1024*1024; // 128 GiB
