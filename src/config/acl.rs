3df77ef5da0306276ed75d80a40aa9f1829e3627
Wolfgang Bumiller <w.bumiller@proxmox.com>
2021-04-22 16:01:57 +0200

    config::acl: make /system/certificates a valid path
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- src/config/acl.rs
+++ src/config/acl.rs
@@ -308,7 +308,7 @@ pub fn check_acl_path(path: &str) -> Result<(), Error> {
                 return Ok(());
             }
             match components[1] {
-                "disks" | "log" | "status" | "tasks" | "time" => {
+                "certificates" | "disks" | "log" | "status" | "tasks" | "time" => {
                     if components_len == 2 {
                         return Ok(());
                     }
