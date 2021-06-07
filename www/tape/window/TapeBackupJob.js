2527c039df0009e7df563188e5acf22234b0427a
Dominik Csapak <d.csapak@proxmox.com>
2021-06-07 15:06:45 +0200

    ui: tape: TapeBackupJob: use correct default value for pbsUserSelector
    if we want the empty value as a valid default value in a combogrid,
    we have to explicitely select 'null' else the field will be marked as
    dirty
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Reported-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/tape/window/TapeBackupJob.js
+++ www/tape/window/TapeBackupJob.js
@@ -70,6 +70,7 @@ Ext.define('PBS.TapeManagement.BackupJobEdit', {
 		fieldLabel: gettext('Notify User'),
 		emptyText: 'root@pam',
 		allowBlank: true,
+		value: null,
 		renderer: Ext.String.htmlEncode,
 	    },
 	],
