67678ec39c53e43bc2077dd23e6026a9fba3f9eb
Wolfgang Bumiller <w.bumiller@proxmox.com>
2021-09-29 12:39:51 +0200

    add all autotraits to output_or_stdout trait object
    just in case we ever need any of them in async code that
    requires them and loses it because of accessing such a trait
    object...
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- pbs-tools/src/cli.rs
+++ pbs-tools/src/cli.rs
@@ -1,13 +1,16 @@
 use std::fs::File;
 use std::io::{self, stdout, Write};
 use std::path::Path;
+use std::panic::{RefUnwindSafe, UnwindSafe};
 
 /// Returns either a new file, if a path is given, or stdout, if no path is given.
-pub fn outfile_or_stdout<P: AsRef<Path>>(path: Option<P>) -> io::Result<Box<dyn Write>> {
+pub fn outfile_or_stdout<P: AsRef<Path>>(
+    path: Option<P>,
+) -> io::Result<Box<dyn Write + Send + Sync + Unpin + RefUnwindSafe + UnwindSafe>> {
     if let Some(path) = path {
         let f = File::create(path)?;
-        Ok(Box::new(f) as Box<dyn Write>)
+        Ok(Box::new(f) as Box<_>)
     } else {
-        Ok(Box::new(stdout()) as Box<dyn Write>)
+        Ok(Box::new(stdout()) as Box<_>)
     }
 }
