7d4d8f47c95810e51fad46ee9818d0e758113f32
Dominik Csapak <d.csapak@proxmox.com>
2021-11-29 15:39:40 +0100

    ui: add GroupSelector
    to select either a group from a datastore
    
    for now it is expected to set the data in the store manually
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ www/form/GroupSelector.js
@@ -0,0 +1,54 @@
+Ext.define('pbs-groups', {
+    extend: 'Ext.data.Model',
+    fields: [
+	'backup-type',
+	'backup-id',
+	{
+	    name: 'group',
+	    type: 'string',
+	    convert: function(value, record) {
+		if (record.data['backup-type'] && record.data['backup-id']) {
+		    return `${record.data['backup-type']}/${record.data['backup-id']}`;
+		}
+		return value;
+	    },
+	},
+    ],
+    proxy: {
+	type: 'proxmox',
+    },
+});
+
+Ext.define('PBS.form.GroupSelector', {
+    extend: 'Proxmox.form.ComboGrid',
+    alias: 'widget.pbsGroupSelector',
+
+    allowBlank: false,
+    autoSelect: false,
+    notFoundIsValid: true,
+    editable: true,
+    valueField: 'group',
+    displayField: 'group',
+
+    store: {
+	sorters: 'group',
+	model: 'pbs-groups',
+    },
+
+    listConfig: {
+	minHeight: 80,
+	emptyText: gettext('No Groups'),
+	viewConfig: {
+	    deferEmptyText: false,
+	},
+	columns: [
+	    {
+		header: gettext('Group'),
+		sortable: true,
+		dataIndex: 'group',
+		renderer: Ext.String.htmlEncode,
+		flex: 1,
+	    },
+	],
+    },
+});
