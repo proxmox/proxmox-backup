118e984996545bdc180c0cc50b63189f33e0d543
Thomas Lamprecht <t.lamprecht@proxmox.com>
2022-05-15 15:59:40 +0200

    datastore: move backup dir/group/namespace iter to own module
    no changes in interface for users of the crate as we re-export
    anyway, so more for avoiding to crowd the datastore module to much
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- pbs-datastore/src/lib.rs
+++ pbs-datastore/src/lib.rs
@@ -206,10 +206,10 @@ pub use manifest::BackupManifest;
 pub use store_progress::StoreProgress;
 
 mod datastore;
-pub use datastore::{
-    check_backup_owner, DataStore, ListGroups, ListNamespaces, ListNamespacesRecursive,
-    ListSnapshots,
-};
+pub use datastore::{check_backup_owner, DataStore};
+
+mod hierarchy;
+pub use hierarchy::{ListGroups, ListNamespaces, ListNamespacesRecursive, ListSnapshots};
 
 mod snapshot_reader;
 pub use snapshot_reader::SnapshotReader;
