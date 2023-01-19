proxmox/proxmox-backup

commit d2aff935c9f356ec7c238c52220a6b86d91fc979

update to nix 0.26
and corresponding proxmox-* dependencies to their versions depending on nix
0.26.

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

pbs-datastore/src/fixed_index.rs
@@ -104,8 +104,8 @@ impl FixedIndexReader {
 
         let data = unsafe {
             nix::sys::mman::mmap(
-                std::ptr::null_mut(),
-                index_size,
+                None,
+                std::num::NonZeroUsize::new(index_size).ok_or_else(|| format_err!("invalid index size"))?,
                 nix::sys::mman::ProtFlags::PROT_READ,
                 nix::sys::mman::MapFlags::MAP_PRIVATE,
                 file.as_raw_fd(),
@@ -290,8 +290,8 @@ impl FixedIndexWriter {
 
         let data = unsafe {
             nix::sys::mman::mmap(
-                std::ptr::null_mut(),
-                index_size,
+                None,
+                std::num::NonZeroUsize::new(index_size).ok_or_else(|| format_err!("invalid index size"))?,
                 nix::sys::mman::ProtFlags::PROT_READ | nix::sys::mman::ProtFlags::PROT_WRITE,
                 nix::sys::mman::MapFlags::MAP_SHARED,
                 file.as_raw_fd(),

Cargo.toml
@@ -63,16 +63,16 @@ proxmox-http = { version = "0.7", features = [ "client", "http-helpers", "websoc
 proxmox-io = "1.0.1" # tools and client use "tokio" feature
 proxmox-lang = "1.1"
 proxmox-metrics = "0.2"
-proxmox-rest-server = "0.2"
+proxmox-rest-server = "0.2.1"
 # some use "cli", some use "cli" and "server", pbs-config uses nothing
-proxmox-router = { version = "1.3.0", default_features = false }
+proxmox-router = { version = "1.3.1", default_features = false }
 # everything but pbs-config and pbs-client ues "api-macro"
-proxmox-schema = "1.3.1"
+proxmox-schema = "1.3.6"
 proxmox-section-config = "1"
 proxmox-serde = "0.1.1"
-proxmox-shared-memory = "0.2"
+proxmox-shared-memory = "0.2.3"
 proxmox-subscription = { version = "0.3", features = [ "api-types" ] }
-proxmox-sys = "0.4.1"
+proxmox-sys = "0.4.2"
 proxmox-tfa = { version = "2.1", features = [ "api", "api-types" ] }
 proxmox-time = "1.1.2"
 proxmox-uuid = "1"
@@ -81,7 +81,7 @@ proxmox-uuid = "1"
 pathpatterns = "0.1.2"
 proxmox-acme-rs = "0.4"
 proxmox-apt = "0.9.0"
-proxmox-openid = "0.9.0"
+proxmox-openid = "0.9.8"
 pxar = "0.10.2"
 
 # PBS workspace
@@ -120,7 +120,7 @@ hyper = { version = "0.14", features = [ "full" ] }
 lazy_static = "1.4"
 libc = "0.2"
 log = "0.4.17"
-nix = "0.24"
+nix = "0.26.1"
 nom = "7"
 num-traits = "0.2"
 once_cell = "1.3.1"

debian/control
@@ -31,7 +31,7 @@ Build-Depends: debhelper (>= 12),
  librust-lazy-static-1+default-dev (>= 1.4-~~),
  librust-libc-0.2+default-dev,
  librust-log-0.4+default-dev (>= 0.4.17-~~),
- librust-nix-0.24+default-dev,
+ librust-nix-0.26+default-dev (>= 0.26.1-~~),
  librust-nom-7+default-dev,
  librust-num-traits-0.2+default-dev,
  librust-once-cell-1+default-dev (>= 1.3.1-~~),
@@ -57,25 +57,25 @@ Build-Depends: debhelper (>= 12),
  librust-proxmox-io-1+tokio-dev (>= 1.0.1-~~),
  librust-proxmox-lang-1+default-dev (>= 1.1-~~),
  librust-proxmox-metrics-0.2+default-dev,
- librust-proxmox-openid-0.9+default-dev,
- librust-proxmox-rest-server-0.2+default-dev,
- librust-proxmox-router-1+cli-dev (>= 1.3-~~),
- librust-proxmox-router-1+default-dev (>= 1.3-~~),
- librust-proxmox-router-1+server-dev (>= 1.3-~~),
- librust-proxmox-schema-1+api-macro-dev (>= 1.3.1-~~),
- librust-proxmox-schema-1+default-dev (>= 1.3.1-~~),
+ librust-proxmox-openid-0.9+default-dev (>= 0.9.8-~~),
+ librust-proxmox-rest-server-0.2+default-dev (>= 0.2.1-~~),
+ librust-proxmox-router-1+cli-dev (>= 1.3.1-~~),
+ librust-proxmox-router-1+default-dev (>= 1.3.1-~~),
+ librust-proxmox-router-1+server-dev (>= 1.3.1-~~),
+ librust-proxmox-schema-1+api-macro-dev (>= 1.3.6-~~),
+ librust-proxmox-schema-1+default-dev (>= 1.3.6-~~),
  librust-proxmox-section-config-1+default-dev,
  librust-proxmox-serde-0.1+default-dev (>= 0.1.1-~~),
  librust-proxmox-serde-0.1+serde-json-dev (>= 0.1.1-~~),
- librust-proxmox-shared-memory-0.2+default-dev,
+ librust-proxmox-shared-memory-0.2+default-dev (>= 0.2.3-~~),
  librust-proxmox-subscription-0.3+api-types-dev,
  librust-proxmox-subscription-0.3+default-dev,
- librust-proxmox-sys-0.4+acl-dev (>= 0.4.1-~~),
- librust-proxmox-sys-0.4+crypt-dev (>= 0.4.1-~~),
- librust-proxmox-sys-0.4+default-dev (>= 0.4.1-~~),
- librust-proxmox-sys-0.4+logrotate-dev (>= 0.4.1-~~),
- librust-proxmox-sys-0.4+sortable-macro-dev (>= 0.4.1-~~),
- librust-proxmox-sys-0.4+timer-dev (>= 0.4.1-~~),
+ librust-proxmox-sys-0.4+acl-dev (>= 0.4.2-~~),
+ librust-proxmox-sys-0.4+crypt-dev (>= 0.4.2-~~),
+ librust-proxmox-sys-0.4+default-dev (>= 0.4.2-~~),
+ librust-proxmox-sys-0.4+logrotate-dev (>= 0.4.2-~~),
+ librust-proxmox-sys-0.4+sortable-macro-dev (>= 0.4.2-~~),
+ librust-proxmox-sys-0.4+timer-dev (>= 0.4.2-~~),
  librust-proxmox-tfa-2+api-dev (>= 2.1-~~),
  librust-proxmox-tfa-2+api-types-dev (>= 2.1-~~),
  librust-proxmox-tfa-2+default-dev (>= 2.1-~~),

commit 9f43a9febf707d75bce8bd5d34e88f683ba075c9

datastore: rustfmt
Fixes: d2aff935 ("update to nix 0.26")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

pbs-datastore/src/fixed_index.rs
@@ -105,7 +105,8 @@ impl FixedIndexReader {
         let data = unsafe {
             nix::sys::mman::mmap(
                 None,
-                std::num::NonZeroUsize::new(index_size).ok_or_else(|| format_err!("invalid index size"))?,
+                std::num::NonZeroUsize::new(index_size)
+                    .ok_or_else(|| format_err!("invalid index size"))?,
                 nix::sys::mman::ProtFlags::PROT_READ,
                 nix::sys::mman::MapFlags::MAP_PRIVATE,
                 file.as_raw_fd(),
@@ -291,7 +292,8 @@ impl FixedIndexWriter {
         let data = unsafe {
             nix::sys::mman::mmap(
                 None,
-                std::num::NonZeroUsize::new(index_size).ok_or_else(|| format_err!("invalid index size"))?,
+                std::num::NonZeroUsize::new(index_size)
+                    .ok_or_else(|| format_err!("invalid index size"))?,
                 nix::sys::mman::ProtFlags::PROT_READ | nix::sys::mman::ProtFlags::PROT_WRITE,
                 nix::sys::mman::MapFlags::MAP_SHARED,
                 file.as_raw_fd(),