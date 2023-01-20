proxmox/proxmox-backup

commit a2055c38fe033cd02bcc6ed093ad1c7d2fcf360c

fix non-camel-case enums
This should have never been started to begin with...

src/api2/config/media_pool.rs
@@ -103,19 +103,18 @@ pub fn get_config(name: String) -> Result<MediaPoolConfig, Error> {
 
 #[api()]
 #[derive(Serialize, Deserialize)]
-#[allow(non_camel_case_types)]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete media set allocation policy.
-    allocation,
+    Allocation,
     /// Delete pool retention policy
-    retention,
+    Retention,
     /// Delete media set naming template
-    template,
+    Template,
     /// Delete encryption fingerprint
-    encrypt,
+    Encrypt,
     /// Delete comment
-    comment,
+    Comment,
 }
 
 #[api(
@@ -158,19 +157,19 @@ pub fn update_pool(
     if let Some(delete) = delete {
         for delete_prop in delete {
             match delete_prop {
-                DeletableProperty::allocation => {
+                DeletableProperty::Allocation => {
                     data.allocation = None;
                 }
-                DeletableProperty::retention => {
+                DeletableProperty::Retention => {
                     data.retention = None;
                 }
-                DeletableProperty::template => {
+                DeletableProperty::Template => {
                     data.template = None;
                 }
-                DeletableProperty::encrypt => {
+                DeletableProperty::Encrypt => {
                     data.encrypt = None;
                 }
-                DeletableProperty::comment => {
+                DeletableProperty::Comment => {
                     data.comment = None;
                 }
             }

src/api2/config/remote.rs
@@ -133,15 +133,14 @@ pub fn read_remote(
 
 #[api()]
 #[derive(Serialize, Deserialize)]
-#[allow(non_camel_case_types)]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete the comment property.
-    comment,
+    Comment,
     /// Delete the fingerprint property.
-    fingerprint,
+    Fingerprint,
     /// Delete the port property.
-    port,
+    Port,
 }
 
 #[api(
@@ -200,13 +199,13 @@ pub fn update_remote(
     if let Some(delete) = delete {
         for delete_prop in delete {
             match delete_prop {
-                DeletableProperty::comment => {
+                DeletableProperty::Comment => {
                     data.config.comment = None;
                 }
-                DeletableProperty::fingerprint => {
+                DeletableProperty::Fingerprint => {
                     data.config.fingerprint = None;
                 }
-                DeletableProperty::port => {
+                DeletableProperty::Port => {
                     data.config.port = None;
                 }
             }

src/api2/node/dns.rs
@@ -22,15 +22,14 @@ static RESOLV_CONF_FN: &str = "/etc/resolv.conf";
 
 #[api()]
 #[derive(Serialize, Deserialize)]
-#[allow(non_camel_case_types)]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete first nameserver entry
-    dns1,
+    Dns1,
     /// Delete second nameserver entry
-    dns2,
+    Dns2,
     /// Delete third nameserver entry
-    dns3,
+    Dns3,
 }
 
 pub fn read_etc_resolv_conf() -> Result<Value, Error> {
@@ -146,13 +145,13 @@ pub fn update_dns(
         for delete_prop in delete {
             let config = config.as_object_mut().unwrap();
             match delete_prop {
-                DeletableProperty::dns1 => {
+                DeletableProperty::Dns1 => {
                     config.remove("dns1");
                 }
-                DeletableProperty::dns2 => {
+                DeletableProperty::Dns2 => {
                     config.remove("dns2");
                 }
-                DeletableProperty::dns3 => {
+                DeletableProperty::Dns3 => {
                     config.remove("dns3");
                 }
             }

src/api2/node/network.rs
@@ -356,13 +356,13 @@ pub fn create_interface(
             if let Some(mode) = bond_mode {
                 interface.bond_mode = bond_mode;
                 if bond_primary.is_some() {
-                    if mode != LinuxBondMode::active_backup {
+                    if mode != LinuxBondMode::ActiveBackup {
                         bail!("bond-primary is only valid with Active/Backup mode");
                     }
                     interface.bond_primary = bond_primary;
                 }
                 if bond_xmit_hash_policy.is_some() {
-                    if mode != LinuxBondMode::ieee802_3ad && mode != LinuxBondMode::balance_xor {
+                    if mode != LinuxBondMode::Ieee802_3ad && mode != LinuxBondMode::BalanceXor {
                         bail!("bond_xmit_hash_policy is only valid with LACP(802.3ad) or balance-xor mode");
                     }
                     interface.bond_xmit_hash_policy = bond_xmit_hash_policy;
@@ -400,40 +400,39 @@ pub fn create_interface(
 
 #[api()]
 #[derive(Serialize, Deserialize)]
-#[allow(non_camel_case_types)]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete the IPv4 address property.
-    cidr,
+    Cidr,
     /// Delete the IPv6 address property.
-    cidr6,
+    Cidr6,
     /// Delete the IPv4 gateway property.
-    gateway,
+    Gateway,
     /// Delete the IPv6 gateway property.
-    gateway6,
+    Gateway6,
     /// Delete the whole IPv4 configuration entry.
-    method,
+    Method,
     /// Delete the whole IPv6 configuration entry.
-    method6,
+    Method6,
     /// Delete IPv4 comments
-    comments,
+    Comments,
     /// Delete IPv6 comments
-    comments6,
+    Comments6,
     /// Delete mtu.
-    mtu,
+    Mtu,
     /// Delete autostart flag
-    autostart,
+    Autostart,
     /// Delete bridge ports (set to 'none')
-    bridge_ports,
+    BridgePorts,
     /// Delete bridge-vlan-aware flag
-    bridge_vlan_aware,
+    BridgeVlanAware,
     /// Delete bond-slaves (set to 'none')
-    slaves,
+    Slaves,
     /// Delete bond-primary
     #[serde(rename = "bond-primary")]
-    bond_primary,
+    BondPrimary,
     /// Delete bond transmit hash policy
-    bond_xmit_hash_policy,
+    BondXmitHashPolicy,
 }
 
 #[api(
@@ -595,49 +594,49 @@ pub fn update_interface(
     if let Some(delete) = delete {
         for delete_prop in delete {
             match delete_prop {
-                DeletableProperty::cidr => {
+                DeletableProperty::Cidr => {
                     interface.cidr = None;
                 }
-                DeletableProperty::cidr6 => {
+                DeletableProperty::Cidr6 => {
                     interface.cidr6 = None;
                 }
-                DeletableProperty::gateway => {
+                DeletableProperty::Gateway => {
                     interface.gateway = None;
                 }
-                DeletableProperty::gateway6 => {
+                DeletableProperty::Gateway6 => {
                     interface.gateway6 = None;
                 }
-                DeletableProperty::method => {
+                DeletableProperty::Method => {
                     interface.method = None;
                 }
-                DeletableProperty::method6 => {
+                DeletableProperty::Method6 => {
                     interface.method6 = None;
                 }
-                DeletableProperty::comments => {
+                DeletableProperty::Comments => {
                     interface.comments = None;
                 }
-                DeletableProperty::comments6 => {
+                DeletableProperty::Comments6 => {
                     interface.comments6 = None;
                 }
-                DeletableProperty::mtu => {
+                DeletableProperty::Mtu => {
                     interface.mtu = None;
                 }
-                DeletableProperty::autostart => {
+                DeletableProperty::Autostart => {
                     interface.autostart = false;
                 }
-                DeletableProperty::bridge_ports => {
+                DeletableProperty::BridgePorts => {
                     set_bridge_ports(interface, Vec::new())?;
                 }
-                DeletableProperty::bridge_vlan_aware => {
+                DeletableProperty::BridgeVlanAware => {
                     interface.bridge_vlan_aware = None;
                 }
-                DeletableProperty::slaves => {
+                DeletableProperty::Slaves => {
                     set_bond_slaves(interface, Vec::new())?;
                 }
-                DeletableProperty::bond_primary => {
+                DeletableProperty::BondPrimary => {
                     interface.bond_primary = None;
                 }
-                DeletableProperty::bond_xmit_hash_policy => interface.bond_xmit_hash_policy = None,
+                DeletableProperty::BondXmitHashPolicy => interface.bond_xmit_hash_policy = None,
             }
         }
     }
@@ -668,13 +667,13 @@ pub fn update_interface(
     if let Some(mode) = bond_mode {
         interface.bond_mode = bond_mode;
         if bond_primary.is_some() {
-            if mode != LinuxBondMode::active_backup {
+            if mode != LinuxBondMode::ActiveBackup {
                 bail!("bond-primary is only valid with Active/Backup mode");
             }
             interface.bond_primary = bond_primary;
         }
         if bond_xmit_hash_policy.is_some() {
-            if mode != LinuxBondMode::ieee802_3ad && mode != LinuxBondMode::balance_xor {
+            if mode != LinuxBondMode::Ieee802_3ad && mode != LinuxBondMode::BalanceXor {
                 bail!("bond_xmit_hash_policy is only valid with LACP(802.3ad) or balance-xor mode");
             }
             interface.bond_xmit_hash_policy = bond_xmit_hash_policy;

pbs-api-types/src/network.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use serde::{Deserialize, Serialize};
 
 use proxmox_schema::*;
@@ -59,42 +61,64 @@ pub enum NetworkConfigMethod {
 #[api()]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
-#[allow(non_camel_case_types)]
 #[repr(u8)]
 /// Linux Bond Mode
 pub enum LinuxBondMode {
     /// Round-robin policy
-    balance_rr = 0,
+    BalanceRr = 0,
     /// Active-backup policy
-    active_backup = 1,
+    ActiveBackup = 1,
     /// XOR policy
-    balance_xor = 2,
+    BalanceXor = 2,
     /// Broadcast policy
-    broadcast = 3,
+    Broadcast = 3,
     /// IEEE 802.3ad Dynamic link aggregation
     #[serde(rename = "802.3ad")]
-    ieee802_3ad = 4,
+    Ieee802_3ad = 4,
     /// Adaptive transmit load balancing
-    balance_tlb = 5,
+    BalanceTlb = 5,
     /// Adaptive load balancing
-    balance_alb = 6,
+    BalanceAlb = 6,
+}
+
+impl fmt::Display for LinuxBondMode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(match self {
+            LinuxBondMode::BalanceRr => "balance-rr",
+            LinuxBondMode::ActiveBackup => "active-backup",
+            LinuxBondMode::BalanceXor => "balance-xor",
+            LinuxBondMode::Broadcast => "broadcast",
+            LinuxBondMode::Ieee802_3ad => "802.3ad",
+            LinuxBondMode::BalanceTlb => "balance-tlb",
+            LinuxBondMode::BalanceAlb => "balance-alb",
+        })
+    }
 }
 
 #[api()]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
-#[allow(non_camel_case_types)]
 #[repr(u8)]
 /// Bond Transmit Hash Policy for LACP (802.3ad)
 pub enum BondXmitHashPolicy {
     /// Layer 2
-    layer2 = 0,
+    Layer2 = 0,
     /// Layer 2+3
     #[serde(rename = "layer2+3")]
-    layer2_3 = 1,
+    Layer2_3 = 1,
     /// Layer 3+4
     #[serde(rename = "layer3+4")]
-    layer3_4 = 2,
+    Layer3_4 = 2,
+}
+
+impl fmt::Display for BondXmitHashPolicy {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(match self {
+            BondXmitHashPolicy::Layer2 => "layer2",
+            BondXmitHashPolicy::Layer2_3 => "layer2+3",
+            BondXmitHashPolicy::Layer3_4 => "layer3+4",
+        })
+    }
 }
 
 #[api()]

pbs-config/src/network/mod.rs
@@ -36,29 +36,9 @@ pub fn bond_mode_from_str(s: &str) -> Result<LinuxBondMode, Error> {
         .map_err(|_: value::Error| format_err!("invalid bond_mode '{}'", s))
 }
 
-pub fn bond_mode_to_str(mode: LinuxBondMode) -> &'static str {
-    match mode {
-        LinuxBondMode::balance_rr => "balance-rr",
-        LinuxBondMode::active_backup => "active-backup",
-        LinuxBondMode::balance_xor => "balance-xor",
-        LinuxBondMode::broadcast => "broadcast",
-        LinuxBondMode::ieee802_3ad => "802.3ad",
-        LinuxBondMode::balance_tlb => "balance-tlb",
-        LinuxBondMode::balance_alb => "balance-alb",
-    }
-}
-
 pub fn bond_xmit_hash_policy_from_str(s: &str) -> Result<BondXmitHashPolicy, Error> {
     BondXmitHashPolicy::deserialize(s.into_deserializer())
         .map_err(|_: value::Error| format_err!("invalid bond_xmit_hash_policy '{}'", s))
-}
-
-pub fn bond_xmit_hash_policy_to_str(policy: &BondXmitHashPolicy) -> &'static str {
-    match policy {
-        BondXmitHashPolicy::layer2 => "layer2",
-        BondXmitHashPolicy::layer2_3 => "layer2+3",
-        BondXmitHashPolicy::layer3_4 => "layer3+4",
-    }
 }
 
 // Write attributes not depending on address family
@@ -78,21 +58,17 @@ fn write_iface_attributes(iface: &Interface, w: &mut dyn Write) -> Result<(), Er
             }
         }
         NetworkInterfaceType::Bond => {
-            let mode = iface.bond_mode.unwrap_or(LinuxBondMode::balance_rr);
-            writeln!(w, "\tbond-mode {}", bond_mode_to_str(mode))?;
+            let mode = iface.bond_mode.unwrap_or(LinuxBondMode::BalanceRr);
+            writeln!(w, "\tbond-mode {mode}")?;
             if let Some(primary) = &iface.bond_primary {
-                if mode == LinuxBondMode::active_backup {
+                if mode == LinuxBondMode::ActiveBackup {
                     writeln!(w, "\tbond-primary {}", primary)?;
                 }
             }
 
             if let Some(xmit_policy) = &iface.bond_xmit_hash_policy {
-                if mode == LinuxBondMode::ieee802_3ad || mode == LinuxBondMode::balance_xor {
-                    writeln!(
-                        w,
-                        "\tbond_xmit_hash_policy {}",
-                        bond_xmit_hash_policy_to_str(xmit_policy)
-                    )?;
+                if mode == LinuxBondMode::Ieee802_3ad || mode == LinuxBondMode::BalanceXor {
+                    writeln!(w, "\tbond_xmit_hash_policy {xmit_policy}")?;
                 }
             }
 

src/api2/access/user.rs
@@ -189,16 +189,15 @@ pub fn read_user(userid: Userid, rpcenv: &mut dyn RpcEnvironment) -> Result<User
 #[api()]
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
-#[allow(non_camel_case_types)]
 pub enum DeletableProperty {
     /// Delete the comment property.
-    comment,
+    Comment,
     /// Delete the firstname property.
-    firstname,
+    Firstname,
     /// Delete the lastname property.
-    lastname,
+    Lastname,
     /// Delete the email property.
-    email,
+    Email,
 }
 
 #[api(
@@ -261,10 +260,10 @@ pub fn update_user(
     if let Some(delete) = delete {
         for delete_prop in delete {
             match delete_prop {
-                DeletableProperty::comment => data.comment = None,
-                DeletableProperty::firstname => data.firstname = None,
-                DeletableProperty::lastname => data.lastname = None,
-                DeletableProperty::email => data.email = None,
+                DeletableProperty::Comment => data.comment = None,
+                DeletableProperty::Firstname => data.firstname = None,
+                DeletableProperty::Lastname => data.lastname = None,
+                DeletableProperty::Email => data.email = None,
             }
         }
     }

src/api2/config/access/openid.rs
@@ -146,21 +146,20 @@ pub fn read_openid_realm(
 #[api()]
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
-#[allow(non_camel_case_types)]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete the client key.
-    client_key,
+    ClientKey,
     /// Delete the comment property.
-    comment,
+    Comment,
     /// Delete the autocreate property
-    autocreate,
+    Autocreate,
     /// Delete the scopes property
-    scopes,
+    Scopes,
     /// Delete the prompt property
-    prompt,
+    Prompt,
     /// Delete the acr_values property
-    acr_values,
+    AcrValues,
 }
 
 #[api(
@@ -215,22 +214,22 @@ pub fn update_openid_realm(
     if let Some(delete) = delete {
         for delete_prop in delete {
             match delete_prop {
-                DeletableProperty::client_key => {
+                DeletableProperty::ClientKey => {
                     config.client_key = None;
                 }
-                DeletableProperty::comment => {
+                DeletableProperty::Comment => {
                     config.comment = None;
                 }
-                DeletableProperty::autocreate => {
+                DeletableProperty::Autocreate => {
                     config.autocreate = None;
                 }
-                DeletableProperty::scopes => {
+                DeletableProperty::Scopes => {
                     config.scopes = None;
                 }
-                DeletableProperty::prompt => {
+                DeletableProperty::Prompt => {
                     config.prompt = None;
                 }
-                DeletableProperty::acr_values => {
+                DeletableProperty::AcrValues => {
                     config.acr_values = None;
                 }
             }

commit 9936c03a52f2fbfad8407872444073603c6cb47f

api: serde-rename deleteable properties to kebab-case
In former commit, the enum members were renamed to be CamelCase, in
accordance with the usual Rust style guide. However, this broke the
GUI in some places due to failing JSON property deserialization.
To fix this, some serde(rename = "kebab-case") directives were added.

Some properties were also serde-renamed to snake_case, otherwise
it would have been necessary to also modify proxmox-widget-toolkit
as well as PVE source code. This can follow in a later commit if so
desired.

Fixes: a2055c38 fix non-camel-case enums
Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>

src/api2/config/media_pool.rs
@@ -103,6 +103,7 @@ pub fn get_config(name: String) -> Result<MediaPoolConfig, Error> {
 
 #[api()]
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete media set allocation policy.

src/api2/config/remote.rs
@@ -133,6 +133,7 @@ pub fn read_remote(
 
 #[api()]
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete the comment property.

src/api2/node/dns.rs
@@ -22,6 +22,7 @@ static RESOLV_CONF_FN: &str = "/etc/resolv.conf";
 
 #[api()]
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete first nameserver entry

src/api2/node/network.rs
@@ -400,6 +400,7 @@ pub fn create_interface(
 
 #[api()]
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 /// Deletable property name
 pub enum DeletableProperty {
     /// Delete the IPv4 address property.
@@ -423,15 +424,17 @@ pub enum DeletableProperty {
     /// Delete autostart flag
     Autostart,
     /// Delete bridge ports (set to 'none')
+    #[serde(rename = "bridge_ports")]
     BridgePorts,
     /// Delete bridge-vlan-aware flag
+    #[serde(rename = "bridge_vlan_aware")]
     BridgeVlanAware,
     /// Delete bond-slaves (set to 'none')
     Slaves,
     /// Delete bond-primary
-    #[serde(rename = "bond-primary")]
     BondPrimary,
     /// Delete bond transmit hash policy
+    #[serde(rename = "bond_xmit_hash_policy")]
     BondXmitHashPolicy,
 }
 