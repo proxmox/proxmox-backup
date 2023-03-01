proxmox/proxmox-backup

commit 139acf37a25cdf395c2974023de31a8ee50b98cd

tape: inventory: skip unassigned tapes
tapes that are labeled into a pool but are not in a media-set yet, belong
to the special 'all zero' media-set. these will never have a catalog on them,
so skip them

fixes the issue, that an inventory with 'catalog restore' aborted on
such a tape

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/api2/tape/drive.rs
@@ -952,17 +952,17 @@ pub fn update_inventory(
                             media_id.label.uuid
                         );
 
-                        if let Some(MediaSetLabel {
-                            ref pool, ref uuid, ..
-                        }) = media_id.media_set_label
-                        {
-                            let _pool_lock = lock_media_pool(TAPE_STATUS_DIR, pool)?;
-                            let _lock = lock_media_set(TAPE_STATUS_DIR, uuid, None)?;
+                        if let Some(ref set) = media_id.media_set_label {
+                            if set.unassigned() {
+                                continue;
+                            }
+                            let _pool_lock = lock_media_pool(TAPE_STATUS_DIR, &set.pool)?;
+                            let _lock = lock_media_set(TAPE_STATUS_DIR, &set.uuid, None)?;
                             MediaCatalog::destroy_unrelated_catalog(TAPE_STATUS_DIR, &media_id)?;
                             inventory.store(media_id.clone(), false)?;
 
                             if catalog {
-                                let media_set = inventory.compute_media_set_members(uuid)?;
+                                let media_set = inventory.compute_media_set_members(&set.uuid)?;
                                 if let Err(err) = fast_catalog_restore(
                                     &worker,
                                     &mut drive,

commit a59ffbbe2ca2d56d3cf30e584b387a12724b7620

tape: inventory: don't skip unassigned tapes completely
since commit 139acf37 ("tape: inventory: skip unassigned tapes")
we skip unassigned tapes (special all-zero media-set uuid) when we look
for a catalog. We accidentally skipped storing it in the inventory
completely, which means we never inventoried completely empty tapes.

to fix that, simply move the check below the inserting in the inventory

Fixes: 139acf37 ("tape: inventory: skip unassigned tapes")

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/api2/tape/drive.rs
@@ -953,13 +953,14 @@ pub fn update_inventory(
                         );
 
                         if let Some(ref set) = media_id.media_set_label {
-                            if set.unassigned() {
-                                continue;
-                            }
                             let _pool_lock = lock_media_pool(TAPE_STATUS_DIR, &set.pool)?;
                             let _lock = lock_media_set(TAPE_STATUS_DIR, &set.uuid, None)?;
                             MediaCatalog::destroy_unrelated_catalog(TAPE_STATUS_DIR, &media_id)?;
                             inventory.store(media_id.clone(), false)?;
+
+                            if set.unassigned() {
+                                continue;
+                            }
 
                             if catalog {
                                 let media_set = inventory.compute_media_set_members(&set.uuid)?;