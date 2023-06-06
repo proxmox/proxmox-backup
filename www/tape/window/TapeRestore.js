proxmox/proxmox-backup

commit df881ed0fc89a0c008bfb6281896fc8c20da7005

ui: tape: fix restoring a single datastore
if the user selected a single datastore to restore from tape on a
media-set that includes more than one, the ui suggests (and is
intended to) that only that datastore will be restored on the selected
target.

The logic to construct the store mapping parameter did not consider that
case, and simply gave the target as default store, which meant all
datastores were restored onto the target.

To fix that, we have to get the source datastore from the field, and
construct the correct parameter ("source=target").

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

www/tape/window/TapeRestore.js
@@ -375,9 +375,15 @@ Ext.define('PBS.TapeManagement.TapeRestoreWindow', {
 		    onGetValues: function(values) {
 			let me = this;
 			let controller = me.up('window').getController();
+			let vm = controller.getViewModel();
 			let datastores = [];
 			if (values.store.toString() !== "") {
-			    datastores.push(values.store);
+			    if (vm.get('singleDatastore')) {
+				let source = controller.lookup('snapshotGrid').getValue();
+				datastores.push(`${source}=${values.store}`);
+			    } else {
+				datastores.push(values.store);
+			    }
 			    delete values.store;
 			}
 

commit 73bd988c42859836676c707c3b5257bad40a199f

ui: tape: fix restore datastore mapping parameter construction
We recently took into account the selected datastore when restoring
from tape, but the snapshot grids value may not only be a single
datastore, it can also be a list of snapshots, datastores or 'all'.

Handle these cases and extract the source datastore correctly.

This fixes tape restoration when not a whole datastore is selected.

Reported in the forum:
https://forum.proxmox.com/threads/restore-from-lto-parameter-verification-errors-store.128445

Fixes: df881ed0 ("ui: tape: fix restoring a single datastore")
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

www/tape/window/TapeRestore.js
@@ -380,6 +380,19 @@ Ext.define('PBS.TapeManagement.TapeRestoreWindow', {
 			if (values.store.toString() !== "") {
 			    if (vm.get('singleDatastore')) {
 				let source = controller.lookup('snapshotGrid').getValue();
+				if (source === 'all') {
+				    // all values are selected
+				    source = values.store;
+				} else if (Ext.isArray(source)) {
+				    if (source[0].indexOf(':') !== -1) {
+					// one or multiple snapshots are selected
+					// extract datastore from first
+					source = source[0].split(':')[0];
+				    } else {
+					// one whole datstore is selected
+					source = source[0];
+				    }
+				}
 				datastores.push(`${source}=${values.store}`);
 			    } else {
 				datastores.push(values.store);