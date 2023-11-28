proxmox/proxmox-backup

commit 9039d6709e375f37bb8a21680f6fefb18ee87ab3

ui: fix sync job initial loading of local datastores
Signed-off-by: Hannes Laimer <h.laimer@proxmox.com>

www/window/SyncJobEdit.js
@@ -188,6 +188,7 @@ Ext.define('PBS.window.SyncJobEdit', {
 				if (isLocalSync) {
 				    storeField.setDisabled(false);
 				    rateLimitField.setValue(null);
+				    storeField.setRemote(null);
 				} else {
 				    remoteField.validate();
 				}

commit fbee479979a246c2b233e9999166388a237c1c0b

ui: fix local sync job edit
... since the store field was cleared when the window opened.

Reported-by: Lukas Wagner <l.wagner@proxmox.com>
Fixes: 9039d6709e375f37bb8a21680f6fefb18ee87ab3
Signed-off-by: Hannes Laimer <h.laimer@proxmox.com>
Tested-by: Lukas Wagner <l.wagner@proxmox.com>

www/form/RemoteTargetSelector.js
@@ -33,10 +33,10 @@ Ext.define('PBS.form.RemoteStoreSelector', {
 	// do nothing.
     },
 
-    setRemote: function(remote) {
+    setRemote: function(remote, forceReload = false) {
 	let me = this;
 
-	if (me.remote === remote) {
+	if (!forceReload && me.remote === remote) {
 	    return;
 	}
 

www/window/SyncJobEdit.js
@@ -181,15 +181,15 @@ Ext.define('PBS.window.SyncJobEdit', {
 				remoteField.setDisabled(isLocalSync);
 				storeField.setDisabled(!isLocalSync && !remoteField.value);
 				if (isLocalSync === !!remoteField.value) {
-				    storeField.clearValue();
 				    remoteField.clearValue();
 				}
 
 				if (isLocalSync) {
 				    storeField.setDisabled(false);
 				    rateLimitField.setValue(null);
-				    storeField.setRemote(null);
+				    storeField.setRemote(null, true);
 				} else {
+				    storeField.clearValue();
 				    remoteField.validate();
 				}
 			    },