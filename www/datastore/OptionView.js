proxmox/proxmox-backup

commit e9979a1ac6b4106b2648b28bb0ab361619ff9791

ui: add 'keep configs' checkbox to datastore removal window
... since the API already accepts a boolean for that.

Signed-off-by: Hannes Laimer <h.laimer@proxmox.com>
[ DC: actually send the option to the api ]
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

www/datastore/OptionView.js
@@ -34,7 +34,10 @@ Ext.define('PBS.window.SafeDatastoreDestroy', {
     getParams: function() {
 	let viewModel = this.getViewModel();
 
-	let params = { 'destroy-data': viewModel.get('destroyData') };
+	let params = {
+	    'destroy-data': viewModel.get('destroyData'),
+	    'keep-job-configs': viewModel.get('keepJobConfigs'),
+	};
 	return `?${Ext.Object.toQueryString(params)}`;
     },
     additionalItems: [{
@@ -44,6 +47,14 @@ Ext.define('PBS.window.SafeDatastoreDestroy', {
 	defaultValue: false,
 	bind: {
 	    value: '{destroyData}',
+	},
+    }, {
+	xtype: 'proxmoxcheckbox',
+	name: 'keep-job-configs',
+	boxLabel: gettext("Keep configured jobs and permissions"),
+	defaultValue: false,
+	bind: {
+	    value: '{keepJobConfigs}',
 	},
     }, {
 	xtype: 'component',

commit 6ee8e0917dc35605af8642473e3718562b45ed9f

ui: fix deleting datastore without 'keep-job-configs'
without any default value in the viewModel, the resulting url would be:

`<id>?destroy-data=<value>&keep-job-configs=`

which is missing the actual value, so add the default

Fixes: e9979a1a ("ui: add 'keep configs' checkbox to datastore removal window")
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Fiona Ebner <f.ebner@proxmox.com>

www/datastore/OptionView.js
@@ -10,6 +10,7 @@ Ext.define('PBS.window.SafeDatastoreDestroy', {
     viewModel: {
 	data: {
 	    'destroyData': 0,
+	    'keepJobConfigs': 0,
 	},
 	formulas: {
 	    destroyNote: get => get('destroyData')