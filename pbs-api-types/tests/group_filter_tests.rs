3a45719ecf1682c955286b175d981560921e455b
Wolfgang Bumiller <w.bumiller@proxmox.com>
2024-01-04 12:10:12 +0100

    move api-types tests to api-types and drop vec![] macro
    we don't need to allocate here
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- pbs-api-types/tests/group_filter_tests.rs
+++ pbs-api-types/tests/group_filter_tests.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 fn test_no_filters() {
     let group_filters = vec![];
 
-    let do_backup = vec![
+    let do_backup = [
         "vm/101", "vm/102", "vm/103", "vm/104", "vm/105", "vm/106", "vm/107", "vm/108", "vm/109",
     ];
 
@@ -18,11 +18,11 @@ fn test_no_filters() {
 fn test_include_filters() {
     let group_filters = vec![GroupFilter::from_str("regex:.*10[2-8]").unwrap()];
 
-    let do_backup = vec![
+    let do_backup = [
         "vm/102", "vm/103", "vm/104", "vm/105", "vm/106", "vm/107", "vm/108",
     ];
 
-    let dont_backup = vec!["vm/101", "vm/109"];
+    let dont_backup = ["vm/101", "vm/109"];
 
     for id in do_backup {
         assert!(BackupGroup::new(BackupType::Vm, id).apply_filters(&group_filters));
@@ -35,14 +35,14 @@ fn test_include_filters() {
 
 #[test]
 fn test_exclude_filters() {
-    let group_filters = vec![
+    let group_filters = [
         GroupFilter::from_str("exclude:regex:.*10[1-3]").unwrap(),
         GroupFilter::from_str("exclude:regex:.*10[5-7]").unwrap(),
     ];
 
-    let do_backup = vec!["vm/104", "vm/108", "vm/109"];
+    let do_backup = ["vm/104", "vm/108", "vm/109"];
 
-    let dont_backup = vec!["vm/101", "vm/102", "vm/103", "vm/105", "vm/106", "vm/107"];
+    let dont_backup = ["vm/101", "vm/102", "vm/103", "vm/105", "vm/106", "vm/107"];
 
     for id in do_backup {
         assert!(BackupGroup::new(BackupType::Vm, id).apply_filters(&group_filters));
@@ -54,15 +54,15 @@ fn test_exclude_filters() {
 
 #[test]
 fn test_include_and_exclude_filters() {
-    let group_filters = vec![
+    let group_filters = [
         GroupFilter::from_str("exclude:regex:.*10[1-3]").unwrap(),
         GroupFilter::from_str("regex:.*10[2-8]").unwrap(),
         GroupFilter::from_str("exclude:regex:.*10[5-7]").unwrap(),
     ];
 
-    let do_backup = vec!["vm/104", "vm/108"];
+    let do_backup = ["vm/104", "vm/108"];
 
-    let dont_backup = vec![
+    let dont_backup = [
         "vm/101", "vm/102", "vm/103", "vm/105", "vm/106", "vm/107", "vm/109",
     ];
 
