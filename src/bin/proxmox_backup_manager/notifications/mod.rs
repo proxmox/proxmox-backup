ddc94b03c2546a21ad75d4a617209b684a9be929
Lukas Wagner <l.wagner@proxmox.com>
2024-04-23 13:52:25 +0200

    proxmox-backup-manager: add CLI for SMTP endpoints
    Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>
    Tested-by: Gabriel Goller <g.goller@proxmox.com>
    Reviewed-by: Gabriel Goller <g.goller@proxmox.com>
    Tested-by: Maximiliano Sandoval <m.sandoval@proxmox.com>
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/bin/proxmox_backup_manager/notifications/mod.rs
+++ src/bin/proxmox_backup_manager/notifications/mod.rs
@@ -3,12 +3,14 @@ use proxmox_router::cli::{CliCommandMap, CommandLineInterface};
 mod gotify;
 mod matchers;
 mod sendmail;
+mod smtp;
 mod targets;
 
 pub fn notification_commands() -> CommandLineInterface {
     let endpoint_def = CliCommandMap::new()
         .insert("gotify", gotify::commands())
-        .insert("sendmail", sendmail::commands());
+        .insert("sendmail", sendmail::commands())
+        .insert("smtp", smtp::commands());
 
     let cmd_def = CliCommandMap::new()
         .insert("endpoint", endpoint_def)
